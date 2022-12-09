e32d527050db6523f4538d7ac173142e10ee8519
AnastasiaThunderfoot <68298206+AnastasiaTkachenko@users.noreply.github.com>
2022-12-09 14:27:44 +0100

    validation of the buttons

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,30 @@
 /* document.getElementById("main-action").onclick = function () {
    alert(1);
 }; */
-
+/* одна кнопка  */
 document.getElementById("main-action").onclick = function () {
   document.getElementById("cars").scrollIntoView({ behavior: "smooth" });
 };
-
+/* несколько кнопок  */
 var buttons = document.getElementsByClassName("car-button");
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].onclick = function () {
     document.getElementById("price").scrollIntoView({ behavior: "smooth" });
   };
 }
+
+/* валидация полей  */
+document.getElementById("price-action").onclick = function () {
+  if (document.getElementById("name").value === "") {
+    alert("Заполните поле имя");
+  } else if (document.getElementById("phone").value === "") {
+    alert("Заполните поле телефон");
+  } else if (document.getElementById("car").value === "") {
+    alert("Заполните поле автомобиль");
+  } else {
+    alert("Спасибо за заявку мы,свяжемся с Вами в ближайшее время");
+  }
+};
 /* parallax */
 
 document.addEventListener("DOMContentLoaded", function () {
