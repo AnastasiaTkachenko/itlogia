c661612361a48ef52a7bdcc264ef16ed64b75abf
AnastasiaThunderfoot <68298206+AnastasiaTkachenko@users.noreply.github.com>
2022-12-09 13:37:08 +0100

    javascript smooth behavior oncllick

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,34 @@
+/* document.getElementsByClassName("main-title")[0].style.color = "red"; */
 
+/* document.getElementById("main-action").onclick = function () {
+   alert(1);
+}; */
 
+document.getElementById("main-action").onclick = function () {
+  document.getElementById("cars").scrollIntoView({ behavior: "smooth" });
+};
 
-
-
-
-
-
-
-
+var buttons = document.getElementsByClassName("car-button");
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = function () {
+    document.getElementById("price").scrollIntoView({ behavior: "smooth" });
+  };
+}
 /* parallax */
 
 document.addEventListener("DOMContentLoaded", function () {
-   let layer = document.querySelector('.price-image');
-document.addEventListener('mousemove', (event) => {
-       layer.style.transform = 'translate3d(' + ((event.clientX * 0.5) / 10) + 'px,' + ((event.clientY * 0.2) / 8) + 'px,0px)';
-});
+  let layer = document.querySelector(".price-image");
+  document.addEventListener("mousemove", (event) => {
+    layer.style.transform =
+      "translate3d(" +
+      (event.clientX * 0.5) / 10 +
+      "px," +
+      (event.clientY * 0.2) / 8 +
+      "px,0px)";
+  });
 
-   const elem = document.querySelector(".main");
-document.addEventListener('scroll', () => {
-       elem.style.backgroundPositionX = '0' + (0.2 * window.pageYOffset) + 'px';
-})
+  const elem = document.querySelector(".main");
+  document.addEventListener("scroll", () => {
+    elem.style.backgroundPositionX = "0" + 0.2 * window.pageYOffset + "px";
+  });
 });
